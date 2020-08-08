fcc3758cf57569077be8d5b50c9426158479ca3a
sundaydavid <sundaydavid989@gmail.com>
2020-08-08 21:37:33 +0100

    fetch male cloths data

--- app/src/main/java/com/mjslick/ui/adapters/MaleClothsAdapter.kt
+++ app/src/main/java/com/mjslick/ui/adapters/MaleClothsAdapter.kt
@@ -1,6 +1,5 @@
 package com.mjslick.ui.adapters
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,15 +10,15 @@ import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.mjslick.R
-import com.mjslick.model.LadiesWear
+import com.mjslick.model.MaleWear
 
-class MaleClothsAdapter(private val clothList: List<LadiesWear>)
+class MaleClothsAdapter(private val clothList: List<MaleWear>)
     :RecyclerView.Adapter<MaleClothsAdapter.ViewHolder>(){
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.female_wear_list_item,
+            LayoutInflater.from(parent.context).inflate(R.layout.male_wear_list_item,
             parent, false)
         return ViewHolder(view)
     }
@@ -30,19 +29,19 @@ class MaleClothsAdapter(private val clothList: List<LadiesWear>)
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
        val data = clothList[position]
         holder.loadImage(data.clothImages[0])
-        holder.femaleWearTitle.text = data.clothName
-        holder.femaleWearRating.numStars = 5
-        holder.femaleWearRating.rating = 4F
+        holder.maleWearTitle.text = data.clothName
+        holder.maleWearRating.numStars = 5
+        holder.maleWearRating.rating = 4F
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-         val femaleWearTitle: TextView = itemView.findViewById(R.id.female_wear_title)
-         val femaleWearRating: RatingBar = itemView.findViewById(R.id.female_wear_rating)
-        private val femaleWearImage: ImageView = itemView.findViewById(R.id.ladies_wear_image)
+         val maleWearTitle: TextView = itemView.findViewById(R.id.male_wear_title)
+         val maleWearRating: RatingBar = itemView.findViewById(R.id.male_wear_rating)
+        private val maleWearImage: ImageView = itemView.findViewById(R.id.male_wear_image)
 
         init {
             itemView.setOnClickListener {
-                Toast.makeText(itemView.context, clothList[adapterPosition].toString(),
+                Toast.makeText(itemView.context, adapterPosition.toString(),
                     Toast.LENGTH_SHORT).show()
             }
         }
@@ -50,7 +49,7 @@ class MaleClothsAdapter(private val clothList: List<LadiesWear>)
         fun loadImage(image: String) {
             Glide.with(itemView.context)
                 .load(image)
-                .into(femaleWearImage)
+                .into(maleWearImage)
         }
 
     }
--- app/src/main/java/com/mjslick/ui/male/MaleFragment.kt
+++ app/src/main/java/com/mjslick/ui/male/MaleFragment.kt
@@ -1,31 +1,30 @@
 package com.mjslick.ui.male
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.GridLayoutManager
 import com.mjslick.R
-import com.mjslick.ui.factory.GetFemaleClothFactory
+import com.mjslick.ui.adapters.MaleClothsAdapter
 import com.mjslick.ui.factory.GetMaleClothFactory
-import com.mjslick.ui.female.FemaleViewModel
 import kotlinx.android.synthetic.main.male_fragment.view.*
 
 class MaleFragment : Fragment() {
 
 
     private lateinit var viewModel: MaleViewModel
-
+    private lateinit var adapter: MaleClothsAdapter
+    private lateinit var root: View
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.male_fragment, container, false)
-
+         root = inflater.inflate(R.layout.male_fragment, container, false)
 
         val application = requireNotNull(this.activity).application
         val viewModelFactory = GetMaleClothFactory(application)
@@ -34,7 +33,18 @@ class MaleFragment : Fragment() {
         root.add_male_wear.setOnClickListener {
             Navigation.findNavController(root).navigate(R.id.navigation_addMaleCloth)
         }
+        displayCloths()
         return root
+    }
+
+    private fun displayCloths(){
+        root.male_wear_recyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
+        viewModel.getMaleCloths {wears->
+            adapter = MaleClothsAdapter(wears)
+            root.male_wear_recyclerView.adapter = adapter
+            adapter.notifyDataSetChanged()
+
+        }
     }
 
 }
